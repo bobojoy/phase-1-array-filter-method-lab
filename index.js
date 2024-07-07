2a3078a8238f1b8732adc7c42c02b3b61a805d95
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-07-07 12:54:08 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const names =["Joy", "Sam", "Bobby", "Cess", "Jimmy"];
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+console.log(findMatching())
+
+function fuzzyMatch(drivers,name){
+    return drivers.filter(driver => driver[0] === name[0])
+}
+
+function matchName(drivers,name){
+  
+    const matches= drivers.filter(driver => driver.name === name);
+
+    return matches
+}
